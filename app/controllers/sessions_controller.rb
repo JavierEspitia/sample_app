fa71c1c9d02d8259b21f98d3cc3c11e3b7fa0548
Javier Espitia <javi.espitiad@gmail.com>
2021-12-06 10:22:28 -0500

    Implement basic login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  
+  def new
+  end
+  
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      reset_session #metodo de rails
+      log_in user #metodo del helper
+      redirect_to user_url(user)
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+  
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+
+end
